4a205f63b49c5bfbca200927da4663fbe3154f18
ovenoboyo <ovenoboyo@gmail.com>
2021-10-27 20:37:25 +0530

    Seperate method to get auth header

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,19 +8,25 @@ export async function post(url: string, data: any, token?: string, stringify = t
 
     return fetch(BASE_URL + url, {
         method: 'POST',
-        headers: headers,
         body: stringify ? JSON.stringify(data) : data,
+        ...getAuthHeader(token),
     })
 }
 
 export async function get(url: string, token?: string) {
+    return fetch(BASE_URL + url, {
+        method: 'GET',
+        ...getAuthHeader(token)
+    })
+}
+
+function getAuthHeader(token?: string) {
     const headers: any = {}
     if (token) {
         headers["authorization"] = 'Bearer ' + token
     }
 
-    return fetch(BASE_URL + url, {
-        method: 'GET',
+    return {
         headers: headers,
-    })
+    }
 }
